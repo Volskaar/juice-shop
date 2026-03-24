91f4eb5324dc46427c605bfe6638030932ec4b21
erik.wohlrab <cs24m001@technikum-wien.at>
2026-03-25 00:14:56 +0100

    Fix another possible SQL injection entryway

--- routes/search.ts
+++ routes/search.ts
@@ -18,10 +18,17 @@ class ErrorWithParent extends Error {
 // vuln-code-snippet start unionSqlInjectionChallenge dbSchemaChallenge
 export function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
-    let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
+    let criteria: string = req.query.q === 'undefined' ? '' : (req.query.q as string) ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
-      .then(([products]: any) => {
+    const searchTerm = `%${criteria}%`;
+    models.sequelize.query(
+      `SELECT * FROM Products
+      WHERE ((name LIKE :search OR description LIKE :search)
+      AND deletedAt IS NULL) ORDER BY name`,
+      {
+        replacements: { search: searchTerm }
+      }
+    ).then(([products]: any) => {
         const dataString = JSON.stringify(products)
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
           let solved = true
